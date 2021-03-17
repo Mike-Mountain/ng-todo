d63098f4d84d6ec097bda8cdade2f8cddaa379b5
Mike <Mikemountain.dev@gmail.com>
2021-03-17 20:30:28 +0200

    Add the authentication module with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
+  {path: 'auth', loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)},
   {path: '', loadChildren: () => import('./modules/task/task.module').then(m => m.TaskModule)}
 ];
 
