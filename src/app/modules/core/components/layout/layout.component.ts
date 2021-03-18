774e519ac882e7c538cfeeed00fc0af1e9f776ca
Mike <Mikemountain.dev@gmail.com>
2021-03-18 11:11:04 +0200

    Set up mennu + routing

--- src/app/modules/core/components/layout/layout.component.ts
+++ src/app/modules/core/components/layout/layout.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  animations: [
+    trigger('toggleMenu', [
+      state('open', style({
+        width: '35%',
+        opacity: '1',
+        zIndex: '10',
+      })),
+      state('closed', style({
+        width: '0',
+        opacity: '0',
+        zIndex: '-10',
+      })),
+      transition('open => closed', animate('.1s linear')),
+      transition('closed => open', animate('.1s linear'))
+    ])
+  ]
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  menuState = 'open';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+    this.menuState === 'closed' ?
+      this.menuState = 'open' :
+      this.menuState = 'closed';
   }
 
 }
